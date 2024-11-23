624eb420373da27bcdb665185bf82ec72f4544ac
siddhartha reddy <reddynarra143@gmail.com>
2024-11-23 13:29:42 +0530

    secondjava

--- week-1/first.java
+++ week-1/first.java
@@ -1,5 +1,5 @@
 class first{
     public static void main(String[] args) {
-        System.out.println("hello my teacher name is sathwik");
+        System.out.println("hello my teacher name is siddhartha");
     }
 }
